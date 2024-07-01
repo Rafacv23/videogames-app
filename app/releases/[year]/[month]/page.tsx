2ca06e89ef1d217e3c4b69eb2bece2f72f66bc87
Rafa Canosa <rafacv23@gmail.com>
2024-07-01 16:08:20 +0200

    styles

--- app/releases/[year]/[month]/page.tsx
+++ app/releases/[year]/[month]/page.tsx
@@ -4,8 +4,10 @@ export default function Page({
   params: { year: string; month: string }
 }) {
   return (
-    <div>
-      Games released in {params.year} {params.month}
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        Games released in {params.year} {params.month}
+      </div>
+    </main>
   )
 }
