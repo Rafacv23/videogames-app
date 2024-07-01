e2aa3126c1bc8fbbdc5b6cdd48ced33f9ad8ff7e
Rafa Canosa <rafacv23@gmail.com>
2024-07-01 16:07:49 +0200

    styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} dark`}>
+      <body className={`${inter.className} dark w-2/3 m-auto`}>
         <Header />
         {children}
       </body>
