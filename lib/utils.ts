9cf25fe97526baeaa6fad1914fb6fe4a5309584a
Rafa Canosa <rafacv23@gmail.com>
2024-07-02 17:55:26 +0200

    convert url embed function

--- lib/utils.ts
+++ lib/utils.ts
@@ -26,3 +26,9 @@ export function getCurrentMonthAndYear(): { month: string; year: number } {
 
   return { month, year }
 }
+export function convertYoutubeUrl(url: string) {
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
+  const match = url.match(regExp)
+
+  return match && match[2].length === 11 ? match[2] : null
+}
