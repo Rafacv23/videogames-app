5b500ae25b8f39e8215d0e0f84419dfd8a2659fb
Rafa Canosa <rafacv23@gmail.com>
2024-07-04 02:28:31 +0200

    componetization

--- app/releases/[year]/[month]/page.tsx
+++ app/releases/[year]/[month]/page.tsx
@@ -1,26 +1,16 @@
 import VideogameCard from "@/components/VideogameCard"
 import { Game } from "@/lib/types"
+import { fetchReleasesMonthYear } from "@/lib/utils"
 
 export default async function Page({
   params,
 }: {
   params: { year: string; month: string }
 }) {
-  async function getData({ year, month }: { year: string; month: string }) {
-    // Usar los parámetros pasados para construir la URL de la API
-    const url = `http://localhost:3000/api/releases/${year}/${month}`
-    const res = await fetch(url)
-
-    if (!res.ok) {
-      // Activar el Error Boundary más cercano en caso de fallo
-      throw new Error("Failed to fetch data")
-    }
-
-    return res.json()
-  }
-
-  // Llamar a getData con los parámetros correctos
-  const data = await getData(params)
+  const data = await fetchReleasesMonthYear({
+    year: params.year,
+    month: params.month,
+  })
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
