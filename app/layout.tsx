081998a025ba96981a4879b887a9da44f892f9bb
Rafa Canosa <rafacv23@gmail.com>
2024-07-10 15:42:38 +0200

    new import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import Header from "@/components/Header"
+import Header from "@/components/header/Header"
 import { cn } from "@/lib/utils"
 import { Providers } from "@/components/Providers"
 import { siteConfig } from "@/config/site"
