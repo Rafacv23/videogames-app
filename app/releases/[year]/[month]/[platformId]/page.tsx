4ffb4434da95bf59abed0cf925c34f9270e208ab
Rafa Canosa <rafacv23@gmail.com>
2024-07-10 15:23:51 +0200

    removedunnecesary tags

--- app/releases/[year]/[month]/[platformId]/page.tsx
+++ app/releases/[year]/[month]/[platformId]/page.tsx
@@ -17,25 +17,23 @@ export default async function Page({
   })
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="max-w-5xl w-full mx-auto">
-        <h1 className="text-xl font-bold mb-4">
-          Games in {params.year}/{params.month} for the {params.platformId}
-        </h1>
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
-          {data.games.map((game: Game) => (
-            <li key={game.id}>
-              <VideogameCard game={game} />
-            </li>
-          ))}
-        </ul>
-        <Link href={`#start`}>
-          <Button variant={"outline"}>
-            <MoveUp className="mr-2 h-4 w-4" />
-            Back to top
-          </Button>
-        </Link>
-      </div>
-    </div>
+    <>
+      <h1 className="text-xl font-bold mb-8 mt-8">
+        Games in {params.year}/{params.month} for the {params.platformId}
+      </h1>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
+        {data.games.map((game: Game) => (
+          <li key={game.id}>
+            <VideogameCard game={game} />
+          </li>
+        ))}
+      </ul>
+      <Link href={`#start`}>
+        <Button variant={"outline"}>
+          <MoveUp className="mr-2 h-4 w-4" />
+          Back to top
+        </Button>
+      </Link>
+    </>
   )
 }
