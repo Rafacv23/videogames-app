191e13185d6d89b25c4f2f65c46e846e48a6fba3
Rafa Canosa <rafacv23@gmail.com>
2024-07-10 17:16:15 +0200

    styles

--- components/Search.tsx
+++ components/Search.tsx
@@ -8,8 +8,10 @@ import {
   DialogContent,
   DialogDescription,
   DialogHeader,
+  DialogTitle,
   DialogTrigger,
 } from "./ui/dialog"
+import { Separator } from "./ui/separator"
 const Search = ({ className }: { className?: string }) => {
   const [searchTerm, setSearchTerm] = useState("")
   const [results, setResults] = useState([])
@@ -31,24 +33,32 @@ const Search = ({ className }: { className?: string }) => {
   return (
     <Dialog>
       <DialogTrigger className={className}>Search</DialogTrigger>
-      <DialogContent>
+      <DialogContent className="sm:max-w-md">
         <DialogHeader>
-          <Input
-            type="text"
-            value={searchTerm}
-            onChange={handleChange}
-            placeholder="Search games..."
-          />
+          <DialogTitle>Search</DialogTitle>
+          <DialogDescription>Find your favourite game</DialogDescription>
         </DialogHeader>
-        <DialogDescription>
-          <ul>
-            {results.map((game: Game) => (
+        <Input
+          type="text"
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder="Search games..."
+        />
+        <ul className="flex flex-col gap-4 mt-4">
+          {results.map((game: Game) => (
+            <>
               <li key={game.id}>
-                <a href={`/game/${game.id}`}>{game.name}</a>
+                <a
+                  className="hover:text-pistacho hover:transition-colors"
+                  href={`/game/${game.id}`}
+                >
+                  {game.name}
+                </a>
               </li>
-            ))}
-          </ul>
-        </DialogDescription>
+              <Separator />
+            </>
+          ))}
+        </ul>
       </DialogContent>
     </Dialog>
   )
