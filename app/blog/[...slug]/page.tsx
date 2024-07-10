b7294263bdcec3eabd7179e5420517672d6c312e
Rafa Canosa <rafacv23@gmail.com>
2024-07-10 13:00:34 +0200

    seo

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,5 +1,7 @@
 import { posts } from "#site/content"
 import { MDXContent } from "@/components/blog/mdx-components"
+import { siteConfig } from "@/config/site"
+import { Metadata } from "next"
 import { notFound } from "next/navigation"
 
 interface PostPageProps {
@@ -13,6 +15,46 @@ async function getPostFromParams(params: PostPageProps["params"]) {
   const post = posts.find((post) => post.slugAsParams === slug)
 
   return post
+}
+
+export async function generateMetadata({
+  params,
+}: PostPageProps): Promise<Metadata> {
+  const post = await getPostFromParams(params)
+
+  if (!post) {
+    return {}
+  }
+
+  const ogSearchParams = new URLSearchParams()
+
+  ogSearchParams.set("title", post.title)
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: { name: siteConfig.author },
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      type: "article",
+      url: post.slug,
+      images: [
+        {
+          url: `/api/og?${ogSearchParams.toString()}`,
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: post.title,
+      description: post.description,
+      images: [`/api/og?${ogSearchParams.toString()}`],
+    },
+  }
 }
 
 export async function generateStaticParams(): Promise<
