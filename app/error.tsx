b8f765ecafd51bdc2d01f387b0539f24c31a2015
Rafa Canosa <rafacv23@gmail.com>
2024-07-10 17:16:07 +0200

    error page

---
+++ app/error.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { useEffect } from "react"
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <main className="max-w-3xl mx-auto p-6 h-screen flex flex-col justify-center items-center">
+      <h2 className="mb-4">Something went wrong!</h2>
+      <Button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </main>
+  )
+}
