738a599cda587fa93bf68e16e02a4e171191ac79
Rafa Canosa <rafacv23@gmail.com>
2024-07-10 12:52:05 +0200

    pagination for the blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,27 @@
 import { posts } from "#site/content"
 import { PostItem } from "@/components/blog/post-item"
+import { QueryPagination } from "@/components/blog/query-pagination"
 import { sortPosts } from "@/lib/utils"
 
-export default async function BlogPage() {
+const POSTS_PER_PAGE = 5
+
+interface BlogPageProps {
+  searchParams: {
+    page?: string
+  }
+}
+
+export default async function BlogPage({ searchParams }: BlogPageProps) {
+  const currentPage = Number(searchParams?.page) || 1
   const sortedPosts = sortPosts(posts.filter((post) => post.published))
-  const displayPosts = sortedPosts
+
+  const totalPages = Math.ceil(sortedPosts.length / POSTS_PER_PAGE)
+
+  const displayPosts = sortedPosts.slice(
+    POSTS_PER_PAGE * (currentPage - 1),
+    POSTS_PER_PAGE * currentPage
+  )
+
   return (
     <div className="container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
@@ -35,6 +52,7 @@ export default async function BlogPage() {
       ) : (
         <p>Nothing to see here yet</p>
       )}
+      <QueryPagination totalPages={totalPages} />
     </div>
   )
 }
