c6a465c8eb1f243c5794167eade6a591f76ab073
Rafa Canosa <rafacv23@gmail.com>
2024-07-10 12:29:30 +0200

    site default config

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "VideogameApp",
+  url: "https://example.com",
+  description: "This is a example",
+  author: "Rafa Canosa",
+  links: {
+    github: "https://github.com/rafacv23",
+    personalSite: "https://www.rafacanosa.dev",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
