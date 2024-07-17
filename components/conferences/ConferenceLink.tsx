c8be1a407cc9d374ce566592ea6094c0b028e215
Rafa Canosa <rafacv23@gmail.com>
2024-07-17 16:32:51 +0200

    removed Suspense fallback={<Loading />}>

--- components/conferences/ConferenceLink.tsx
+++ components/conferences/ConferenceLink.tsx
@@ -1,10 +1,9 @@
-import React, { Suspense } from "react"
+import React from "react"
 import { ConferenceLinkProps } from "@/lib/types"
 import { CardDescription } from "@/components/ui/card"
 import Link from "next/link"
 import MyTimer from "../Countdown"
 import { useTranslation } from "react-i18next"
-import Loading from "@/app/[locale]/loading"
 
 const ConferenceLink: React.FC<ConferenceLinkProps> = ({
   nextConference,
@@ -14,28 +13,24 @@ const ConferenceLink: React.FC<ConferenceLinkProps> = ({
   const { t } = useTranslation(locale)
 
   return (
-    <Suspense fallback={<Loading />}>
-      <Link
-        href={
-          nextConference?.url
-            ? `/watch/${year}/${nextConference?.id}`
-            : `/watch/${year}`
-        }
-        className="flex items-center justify-between"
-      >
-        <div>
-          <div className="text-red font-bold flex gap-2 text-sm">
-            {t("conferences:next")}
-            {nextConference?.release_date ? (
-              <MyTimer
-                expiryTimestamp={new Date(nextConference.release_date)}
-              />
-            ) : null}
-          </div>
-          <CardDescription>{nextConference?.name}</CardDescription>
+    <Link
+      href={
+        nextConference?.url
+          ? `/watch/${year}/${nextConference?.id}`
+          : `/watch/${year}`
+      }
+      className="flex items-center justify-between"
+    >
+      <div>
+        <div className="text-red font-bold flex gap-2 text-sm">
+          {t("conferences:next")}
+          {nextConference?.release_date ? (
+            <MyTimer expiryTimestamp={new Date(nextConference.release_date)} />
+          ) : null}
         </div>
-      </Link>
-    </Suspense>
+        <CardDescription>{nextConference?.name}</CardDescription>
+      </div>
+    </Link>
   )
 }
 
