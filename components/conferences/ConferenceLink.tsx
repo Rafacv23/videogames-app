17ffae45b98063ae09e11bacac37e1936f14447a
Rafa Canosa <rafacv23@gmail.com>
2024-07-17 16:22:41 +0200

    add suspense

--- components/conferences/ConferenceLink.tsx
+++ components/conferences/ConferenceLink.tsx
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { ConferenceLinkProps } from "@/lib/types"
 import { CardDescription } from "@/components/ui/card"
 import Link from "next/link"
 import MyTimer from "../Countdown"
 import { useTranslation } from "react-i18next"
+import Loading from "@/app/[locale]/loading"
 
 const ConferenceLink: React.FC<ConferenceLinkProps> = ({
   nextConference,
@@ -13,24 +14,28 @@ const ConferenceLink: React.FC<ConferenceLinkProps> = ({
   const { t } = useTranslation(locale)
 
   return (
-    <Link
-      href={
-        nextConference?.url
-          ? `/watch/${year}/${nextConference?.id}`
-          : `/watch/${year}`
-      }
-      className="flex items-center justify-between"
-    >
-      <div>
-        <div className="text-red font-bold flex gap-2 text-sm">
-          {t("conferences:next")}
-          {nextConference?.release_date ? (
-            <MyTimer expiryTimestamp={new Date(nextConference.release_date)} />
-          ) : null}
+    <Suspense fallback={<Loading />}>
+      <Link
+        href={
+          nextConference?.url
+            ? `/watch/${year}/${nextConference?.id}`
+            : `/watch/${year}`
+        }
+        className="flex items-center justify-between"
+      >
+        <div>
+          <div className="text-red font-bold flex gap-2 text-sm">
+            {t("conferences:next")}
+            {nextConference?.release_date ? (
+              <MyTimer
+                expiryTimestamp={new Date(nextConference.release_date)}
+              />
+            ) : null}
+          </div>
+          <CardDescription>{nextConference?.name}</CardDescription>
         </div>
-        <CardDescription>{nextConference?.name}</CardDescription>
-      </div>
-    </Link>
+      </Link>
+    </Suspense>
   )
 }
 
