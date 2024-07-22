5035c0ba5d32207cb62d5d8f2abf0dca7998b8dc
Rafa Canosa <rafacv23@gmail.com>
2024-07-22 12:52:47 +0200

    added metadata

--- app/[locale]/conferences/layout.tsx
+++ app/[locale]/conferences/layout.tsx
@@ -1,4 +1,21 @@
+import initTranslations from "@/app/i18n"
+import { Metadata, ResolvingMetadata } from "next"
 import React from "react"
+
+type Props = {
+  params: { locale: string }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { t } = await initTranslations(params.locale, ["conferences"])
+  return {
+    title: t("url-title"),
+    description: t("description"),
+  }
+}
 
 export default function ReleasesLayout({
   children,
